03af232a8b823749d78a5663fe30fac7f0840b9e
Dansheen95 <dansheen95@gmail.com>
2017-10-05 23:06:52 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<header>
+
+	<div class="container">
+		<div class="row">
+			<div class="col" id="header-col">
+				<a href="index.php">Accounts Template Daniel Sheen</a>
+			</div>
+			<div class="col" id="header-col">
+
+				<?php
+					if (isset($_SESSION['u_id'])) {
+
+						echo "Hello {$_SESSION['u_uid']}";
+
+						echo '<form action="includes/logout.inc.php" method="POST">
+							<button class="btn btn-primary" type="submit" name="submit">Logout</button>
+						</form>';
+					} else {
+						echo '<form action="includes/login.inc.php" method="POST">
+							<input type="text" name="uid" placeholder="Username/e-mail">
+							<input type="password" name="pwd" placeholder="password">
+							<button type="submit" name="submit">Login</button>
+						</form>
+						<a href="signup.php">Sign up</a>';
+					}
+				?>	
+			</div>
+		</div>
+	</div>
+</header>
